22fed4722daa3ff77598e993ea93f2c24b523d14
Alex <licoms1997@gmail.com>
2023-08-05 19:28:53 +0300

    Add SQL connection to database

---
+++ DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReg_Application
+{
+    class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=LYNX-PC\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if(sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
